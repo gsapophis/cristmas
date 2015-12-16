4da7f035979863843a37f9eab0328d8e73fa3d9a
yaroslavh <yaroslav.polatayko@gmail.com>
2015-12-16 15:45:51 +0200

    fix host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  Rails.application.routes.default_url_options[:host] = 'gooddeal.my-dis.com'
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end

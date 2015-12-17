91cee7b16873e0a5774e4d4da9538c02bcdb570e
NikolayPetrunin <petruninnikolay@gmail.com>
2015-12-17 07:33:16 +0200

    history api

--- app/assets/javascripts/jquery.main.js
+++ app/assets/javascripts/jquery.main.js
@@ -354,7 +354,7 @@
             _makeFacebook = function(){
                 var links = $( '.kid' ).find( '.share_f' ),
                     data = $( '.kid__share' ).data( "kid"),
-                    picture = '&picture=' + data.photo,
+                    picture = '&picture=' + 'http://gooddeal.my-dis.com/assets/baner.jpg',//data.photo,
                     name = '&name=' + data.name,
                     link = '&link=' + location.href,
                     description = '&description=' + $('.kid__description').text(),
